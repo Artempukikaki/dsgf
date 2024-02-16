11a22c8fab97b53198a9af00fd14768e262234b3
Artempukikaki <154607101+Artempukikaki@users.noreply.github.com>
2024-02-16 18:01:20 +0300

    Add files via upload

---
+++ JAVA/тренеры.js
@@ -0,0 +1,6 @@
+const trainers = document.querySelectorAll('.trainer');
+trainers.forEach(trainer => {
+    trainer.addEventListener('click', () => {
+        alert(Выбран ,тренер, $,trainer,querySelector('name'),textContent,);
+    });
+});
